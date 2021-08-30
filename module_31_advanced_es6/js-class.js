95d5adfa7ba65cbd8e42ae5f3acba7946d009144
mhkmcp <mhkmcp@yahoo.com>
2021-08-30 19:01:27 +0600

    31: class added but not finished

---
+++ module_31_advanced_es6/js-class.js
@@ -0,0 +1,10 @@
+class Support {
+    name;
+    designation = "Support Web Dev.";
+    startSession() {
+        console.log('start a Support Session');
+    }
+}
+
+const aamir = new Support('Amir');
+console.log(aamir);
